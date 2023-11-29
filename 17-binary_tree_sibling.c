626158dbbcbd8874641bb240e2b201ae33b50c78
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 12:04:22 -0800

    task17

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_sibling - find nodes siblings
+ * @node: pointer to node
+ * Return: pointer to node's sibling
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	if (node->parent->left == node)
+		return (node->parent->right);
+	return (node->parent->left);
+}
