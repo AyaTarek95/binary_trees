ca358556173f7c0984471c517ba4fb75492157d9
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 08:00:20 -0800

    task1

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_insert_left - insert node as left-child of other node
+ * @parent: pointer to node to insert left-child in
+ * @value: stored in new node
+ * Return: pointer to newnode
+ */
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	if (parent == NULL)
+		{
+		return (NULL);
+		}
+binary_tree_t *newnode;
+
+newnode = malloc(sizeof(binary_tree_t));
+
+	if (newnode == NULL)
+		{
+		return (NULL);
+		}
+newnode->n = value;
+newnode->parent = parent;
+newnode->right = NULL;
+newnode->left = NULL;
+
+	if (parent->left != NULL)
+		{
+		newnode->left = parent->left;
+		parent->left->parent = newnode;
+		}
+
+parent->left = newnode;
+
+return (newnode);
+}
