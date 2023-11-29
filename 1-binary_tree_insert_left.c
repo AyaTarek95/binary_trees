8d4e062e7ef35a48a16a929ee9faf5721a1e6ba3
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 12:20:48 -0800

    fix1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -21,8 +21,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	newnode->n = value;
 	newnode->parent = parent;
-	newnode->right = NULL;
 	newnode->left = NULL;
+	newnode->right = NULL;
 
 	if (parent->left != NULL)
 	{
