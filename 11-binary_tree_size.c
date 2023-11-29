3a69e1494f07ef7283d8645f962fd549632d7abf
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 09:41:36 -0800

    task6

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_size - measure size
+ * @tree: to be measure
+ * Return: size of tree
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+	size += 1;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+	retutn (size);
+}
