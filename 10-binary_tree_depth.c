bc00f2bc7fe9cadd5098b7ab2cf04cf4c51a2390
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 09:34:13 -0800

    task6

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,6 +9,6 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 	if (tree && tree->parent)
-		return (1 + binary_tree_depth(tree->parent);
+		return (1 + binary_tree_depth(tree->parent));
 	return (0);
 }
