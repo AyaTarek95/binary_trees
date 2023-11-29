b9235b3bb5b280427d82ff592ea67a29894ad5c5
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 08:06:36 -0800

    fix1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,29 +8,29 @@
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	if (parent == NULL)
-		{
+	{
 		return (NULL);
-		}
-binary_tree_t *newnode;
+	}
+	binary_tree_t *newnode;
 
-newnode = malloc(sizeof(binary_tree_t));
+	newnode = malloc(sizeof(binary_tree_t));
 
 	if (newnode == NULL)
-		{
+	{
 		return (NULL);
-		}
-newnode->n = value;
-newnode->parent = parent;
-newnode->right = NULL;
-newnode->left = NULL;
+	}
+	newnode->n = value;
+	newnode->parent = parent;
+	newnode->right = NULL;
+	newnode->left = NULL;
 
 	if (parent->left != NULL)
-		{
+	{
 		newnode->left = parent->left;
 		parent->left->parent = newnode;
-		}
+	}
 
-parent->left = newnode;
+	parent->left = newnode;
 
 return (newnode);
 }
