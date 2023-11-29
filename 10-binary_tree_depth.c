72571e960c3c0629e9502d1fbc4396d3badf4c78
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-29 09:33:35 -0800

    task6

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measure tree depth
+ * @tree: to be measured
+ * Return: depth of tree
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+	if (tree && tree->parent)
+		return (1 + binary_tree_depth(tree->parent);
+	return (0);
+}
