c5c261e6fffee57d88d9cddbc41361fca945052a
AyaTarek95 <aya.tarek213@icloud.com>
2023-11-30 11:09:06 -0800

    task14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -24,15 +24,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		size_t h_left = 0, h_right = 0;
 
 		if (tree->left)
-			h_left += 1;
+			h_left = 1 + binary_tree_height(tree->left);
 		else
-			h_left += 0;
-		h_left +=  binary_tree_height(tree->left);
+			h_left = 1;
 		if (tree->right)
-			h_right += 1;
+			h_right = 1 + binary_tree_height(tree->right);
 		else
-			h_right += 0;
-		h_right += binary_tree_height(tree->right);
+			h_right = 1;
 
 		return ((h_left > h_right) ? h_left : h_right);
 	}
